0b2aaa613adbdfbfd4f28d8387524527ba5a0a92
Christoffer Gylin <christoffer@christoffergylin.com>
2025-05-17 13:34:27 +0200

    Created mvnoResponseToInternal function for mapping MVNO rest response to internal api format

--- src/types/Api.ts
+++ src/types/Api.ts
@@ -1,4 +1,4 @@
-import  { z } from 'zod'
+import { z } from 'zod'
 import { mvnoRestResponse } from "../zodSchemas/apiResponses"
 
 export type MvnoRestResponse = z.infer<typeof mvnoRestResponse>
---
+++ src/utils/converters/mvnoResponseToInternal.ts
@@ -0,0 +1,30 @@
+import { type InternalApiFormat, type MvnoRestResponse } from "../../types/Api"
+import { mvnoRestResponse } from "../../zodSchemas/apiResponses"
+
+const mvnoResponseToInternal = (response: MvnoRestResponse): InternalApiFormat => {
+    
+    // Zod is used to validate data in response, if data does not validate correctly an error is thrown
+
+    mvnoRestResponse.parse(response)
+    
+    const internalApiObj: InternalApiFormat = {
+        telgea_user_id: response.user_id,
+        msisdn: response.msisdn,
+        usage_data: {
+            total_mb: response.usage.data.total_mb,
+            roaming_mb: response.usage.data.roaming_mb,
+            country: response.usage.data.country,
+            network_type: response.network.type,
+            provider_code: response.network.provider_code,
+        },
+        sms_charges: [],
+        billing_period: {
+            start: response.usage.period.start,
+            end: response.usage.period.end,
+        }
+    }
+
+    return internalApiObj
+}
+
+export default mvnoResponseToInternal
